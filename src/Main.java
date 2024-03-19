e7b505dc02765cbcd8a756fd124758f4d47797c6
Александр <kmuken@bk.ru>
2024-03-20 00:35:42 +0500

    zdelal

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,38 @@ public class Main {
         return year;
     }
 
+    public static int systemYear () {
+        int clientOS = 0;
+        int clientDeviceYear = 2015;
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+            if (clientDeviceYear < 2015 ) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        }
+        else if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                return clientDeviceYear;
+            }
+
+        }
+        return clientOS;
+    }
 
     public static void main(String[] args) {
 
         // zadanie 1
         int leapYear = 2021;
         checkYear(leapYear);
+
+        //zadanie 2
+        systemYear();
+
+
+
+
 
 
 
